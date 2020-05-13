f2c2ed3acdfb32df9b8d5eee7d18201d2be82637
transcodevops <123548239+transcodevops@users.noreply.github.com>
2020-05-13 10:16:01 +0000

    more specs

--- Shared/ChatGPTAPI.swift
+++ Shared/ChatGPTAPI.swift
@@ -2,4 +2,11 @@
 //  ChatGPTAPI.swift
 //  XCAChatGPT
 //
-//  Created by Alfian Losari on 01/
+//  Created by Alfian Losari on 01/02/23.
+//
+
+import Foundation
+
+class ChatGPTAPI: @unchecked Sendable {
+    
+    private let systemMes
