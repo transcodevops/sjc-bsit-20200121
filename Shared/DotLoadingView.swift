6136826146a23c45b1cbf84b2bffd6ed4c5a4beb
transcodevops <123548239+transcodevops@users.noreply.github.com>
2020-11-21 20:18:00 +0000

    Add some codes

--- Shared/DotLoadingView.swift
+++ Shared/DotLoadingView.swift
@@ -10,3 +10,5 @@ import SwiftUI
 struct DotLoadingView: View {
     
     @State private var showCircle1 = false
+    @State private var showCircle2 = false
+    @State private var showCircle3 = false
